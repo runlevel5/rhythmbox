4f14b02f8c72d41519e7e16d2114356406616437
Trung Lê <8@tle.id.au>
2026-03-08 13:04:27 +1100

    constrain volume button width

--- widgets/rb-header.c
+++ widgets/rb-header.c
@@ -421,7 +421,7 @@ rb_header_constructed (GObject *object)
 	/* volume button */
 	header->priv->volume_button = gtk_volume_button_new ();
 	gtk_widget_set_valign (header->priv->volume_button, GTK_ALIGN_CENTER);
-	gtk_widget_set_hexpand (header->priv->volume_button, FALSE);
+	gtk_widget_set_size_request (header->priv->volume_button, 36, -1);
 	g_signal_connect (header->priv->volume_button, "value-changed",
 			  G_CALLBACK (volume_widget_changed_cb),
 			  header);
